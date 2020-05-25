fe2cdc1d85aa1fd8fa0cec23ed466e6b65e5613d
minhanh555 <65518727+minhanh555@users.noreply.github.com>
2020-05-25 08:15:43 +0700

    lap code

---
+++ frmPhanSo/frmPhanSo/PhanSo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace frmPhanSo
+{
+    public class PhanSo
+    {
+        private int tuSo, mauSo;
+
+        public PhanSo(int t, int m)
+        {
+            this.tuSo = t;
+            this.mauSo = m;
+        }
+
+        public bool sosanh(PhanSo ps)
+        {
+            return true;
+        }
+
+    }
+}
